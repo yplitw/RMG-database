49f6d60d474101c5048b59a256d917d3d7b1f268
PengZhang13 <zhangp@mit.edu>
2016-10-16 19:57:05 -0400

    Add cresol reactions into library phenol_pyr.

--- input/kinetics/libraries/phenol_pyr/reactions.py
+++ input/kinetics/libraries/phenol_pyr/reactions.py
@@ -27,3 +27,38 @@ entry(
     kinetics = Arrhenius(A=(3.36e+12, 's^-1'), n=0.43, Ea=(4.43, 'kcal/mol'), T0=(1, 'K')),
 )
 
+entry(
+    index = 4,
+    label = "cpdylCO_3methyl <=> CPDYL_3methyl + CO",
+    degeneracy = 1,
+    kinetics = Arrhenius(A=(3.36e+12, 's^-1'), n=0.43, Ea=(4.43, 'kcal/mol'), T0=(1, 'K')),
+)
+
+entry(
+    index = 5,
+    label = "cpdylCO_2methyl <=> CPDYL_2methyl + CO",
+    degeneracy = 1,
+    kinetics = Arrhenius(A=(3.36e+12, 's^-1'), n=0.43, Ea=(4.43, 'kcal/mol'), T0=(1, 'K')),
+)
+
+entry(
+    index = 6,
+    label = "cpdylCO_1methyl <=> CPDYL_1methyl + CO",
+    degeneracy = 1,
+    kinetics = Arrhenius(A=(3.36e+12, 's^-1'), n=0.43, Ea=(4.43, 'kcal/mol'), T0=(1, 'K')),
+)
+
+entry(
+    index = 7,
+    label = "cpdylCO_2methoxy <=> CPDYL_2methoxy + CO",
+    degeneracy = 1,
+    kinetics = Arrhenius(A=(3.36e+12, 's^-1'), n=0.43, Ea=(4.43, 'kcal/mol'), T0=(1, 'K')),
+)
+
+entry(
+    index = 8,
+    label = "cpdylCO_1methoxy <=> CPDYL_1methoxy + CO",
+    degeneracy = 1,
+    kinetics = Arrhenius(A=(3.36e+12, 's^-1'), n=0.43, Ea=(4.43, 'kcal/mol'), T0=(1, 'K')),
+)
+
